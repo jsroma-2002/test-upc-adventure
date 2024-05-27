97d52c737bf41d216e00803ebc5a866d49afe45d
Jose Roncal <joseluis.rome@protonmail.com>
2024-05-26 19:53:44 -0500

    fix: :sparkles: add message for no room

--- src/lib/rooms/utils.ts
+++ src/lib/rooms/utils.ts
@@ -1,9 +1,11 @@
 import { getRoomByCoordinates } from "@/services/rooms/room-service";
+import { toast } from "sonner";
 
 export function isOnLimit(positionX: number, positionY: number): boolean {
   const roomFind = getRoomByCoordinates(positionX, positionY);
 
   if (!roomFind) {
+    toast.error("No puedes moverte en esta dirección");
     return false;
   }
 
